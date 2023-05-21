e8bce6023a321240c184488c528c29feeb343d86
asru-islam <arasru01@gmail.com>
2023-05-21 11:26:44 +0600

    install react lottie file

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,15 @@
+import { useRouteError } from "react-router-dom";
+import errorImg from "../../../public/error.json"
 
 const Error = () => {
+    const { error } = useRouteError();
+    console.log(error);
     return (
+      <div className="flex h-screen items-center justify-center">
         <div>
-            Error occur
+          <img src={errorImg} alt="error" />
         </div>
+      </div>
     );
 };
 
